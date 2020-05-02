6f3425e4beac5991f64266be474426dc055076da
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-05-02 14:45:43 -0300

    change icons in onclick button play

--- src/app/pages/js/renderer.js
+++ src/app/pages/js/renderer.js
@@ -1,8 +1,15 @@
 const { ipcRenderer } = require('electron');
 
 let linkSobre = document.querySelector('#link-sobre');
-
+let btnPlay = document.querySelector('.botao-play')
+let sourceImgs = ['img/play-button.svg','img/stop-button.svg']
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
-});
+})
+
+btnPlay.addEventListener('click', function(){
+    sourceImgs.reverse()
+    btnPlay.src = sourceImgs[0]
+})
+
